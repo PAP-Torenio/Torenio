1793cc25c887e8631abbaea4d154b1cd9eed3390
DohManuKii <61553294+DohManuKii@users.noreply.github.com>
2020-03-05 16:10:01 +0000

    alteração manuel

---
+++ pagina inicial/codigo_a_mais.php
@@ -0,0 +1,78 @@
+ <div class="pricing-wrap bg-primary pricing-counter" id="pricing-section" data-aos="fade">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-7 text-center">
+            <h2 class="section-title mb-3 text-center text-white mx-auto">Choose Your Plan</h2>
+            <p class="mb-5 section-desc">Natus totam voluptatibus animi aspernatur ducimus quas obcaecati mollitia quibusdam temporibus culpa dolore molestias blanditiis consequuntur sunt nisi.</p>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div class="row no-gutters">
+        <div class="col-lg-4">
+          <div class="pricing">
+            <h3>Starters</h3>
+            <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <div class="price">
+              <span>Starting at</span>
+              <strong>$<em class="number" data-number="7">0</em></strong>
+              <span>Per Month</span>
+            </div>
+
+            <p class="description-2 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum enim nobis aspernatur saepe dolores?</p>
+
+            <p><a href="#" target="_blank" class="btn btn-outline-primary py-3 px-5">Get Started</a></p>
+            
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="pricing pricing-popular">
+            <h3>Premium</h3>
+            <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <div class="price">
+              <span>Starting at</span>
+              <strong>$<em class="number" data-number="75">0</em></strong>
+              <span>Per Month</span>
+            </div>
+
+            <p class="description-2 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum enim nobis aspernatur saepe dolores?</p>
+
+            <p><a href="#" target="_blank" class="btn  btn-outline-danger py-3 px-5">Get Started</a></p>
+            
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="pricing">
+            <h3>Enterprise</h3>
+            <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <div class="price">
+              <span>Starting at</span>
+              <strong>$<em class="number" data-number="390">0</em></strong>
+              <span>Per Month</span>
+            </div>
+
+            <p class="description-2 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum enim nobis aspernatur saepe dolores?</p>
+
+            <p><a href="#" target="_blank" class="btn btn-outline-primary py-3 px-5">Get Started</a></p>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+	
+	
+	
+	
+	
+	<div class="ml-auto">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+              <ul class="site-menu main-menu site-menu-dark js-clone-nav mr-auto d-none d-xl-block">
+                <li><a href="#pricing-section" class="nav-link">Pricing</a></li>
+                <li class="cta"><a href="#contact-section" class="nav-link"><span class="border bg-danger rounded text-white border-danger">Contact</span></a></li>
+              </ul>
+            </nav>
+            <a href="#" class="d-inline-block d-xl-none site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3"></span></a>
+          </div>
